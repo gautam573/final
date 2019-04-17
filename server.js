e619da327bda26b9b8a80435daf71589376970b1
gautam573@example.com <gautam>
2019-04-16 19:16:43 -0700

    fourth commit

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require ('fs');
 
-var app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (request, response) => {
-   response.send('<h1>Hello Express</h1>');
+
+//app.get('/', (request, response) => {
+  // response.send('<h1>Hello Express</h1>');
+//});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
-
-app.listen(8080);
 
